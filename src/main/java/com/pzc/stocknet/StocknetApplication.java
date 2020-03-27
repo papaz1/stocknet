e5c06d57171cdf36a417d777c2ab39899fb3e34f
Baran <baran.solen@gmail.com>
2020-03-27 08:58:09 +0100

    Initial Version

--- src/main/java/com/pzc/stocknet/StocknetApplication.java
+++ src/main/java/com/pzc/stocknet/StocknetApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StocknetApplication {
 
 	public static void main(String[] args) {
+		//test
 		SpringApplication.run(StocknetApplication.class, args);
 	}
 
