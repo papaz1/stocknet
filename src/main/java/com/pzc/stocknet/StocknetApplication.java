e24a0288dcd89331c62bd8d04f7c3cf402b16220
Baran <baran.solen@gmail.com>
2020-03-27 08:55:25 +0100

    Initial Version

---
+++ src/main/java/com/pzc/stocknet/StocknetApplication.java
@@ -0,0 +1,13 @@
+package com.pzc.stocknet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocknetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocknetApplication.class, args);
+	}
+
+}
